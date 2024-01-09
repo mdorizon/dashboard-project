e3a60c3088d58b688a07d655c39ddda7886e0475
mdorizon <mdorizon17@gmail.com>
2024-01-09 23:00:57 +0100

    dynamic boards & columns

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { Board } from '../models/board.model';
+import { Column } from '../models/column.model';
 
 @Component({
   selector: 'app-kanban',
@@ -8,11 +10,27 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList }
 
 })
 export class KanbanComponent {
-  idea = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
   
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  board: Board = new Board('Test board', [
+    new Column('Ideas', [
+      "Some random idea",
+      "This is another random idea",
+      "build an awesome application"
+    ]),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep'
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ])
+  ]);
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
