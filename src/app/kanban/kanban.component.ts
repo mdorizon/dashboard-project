c2aa3a4f509eeb64a4e2e0041cf9e1dede276300
mdorizon <mdorizon17@gmail.com>
2024-01-09 22:47:41 +0100

    tasks list

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -8,6 +8,8 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList }
 
 })
 export class KanbanComponent {
+  idea = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  
   todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
