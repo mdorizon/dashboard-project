26f6445eb94afd6fad4227595df589d37237bc5f
mdorizon <mdorizon17@gmail.com>
2024-01-09 16:23:05 +0100

    repaired

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-kanban',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './kanban.component.scss'
 })
 export class KanbanComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
