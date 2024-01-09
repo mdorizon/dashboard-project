4e343fa4811d912edc0790fd51ba09c1bad39bc0
Pitra <justin.pitra@gmail.com>
2024-01-09 22:00:57 +0100

    created form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
