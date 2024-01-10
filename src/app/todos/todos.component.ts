25eb2f1e949f70e234ac295a324489dfc5e39fb3
Pitra <justin.pitra@gmail.com>
2024-01-10 11:58:21 +0100

    adding error msg for todoTextField

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../shared/todo.model'
 import { DataService } from '../shared/data.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todos',
@@ -10,10 +11,21 @@ import { DataService } from '../shared/data.service';
 export class TodosComponent implements OnInit{
 
   todos : Todo[]
+  showValidationErrors: boolean
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.todos = this.dataService.getAllTodos()
+  }
+
+  onFormSubmit(form: NgForm) {
+    if(form.invalid) return this.showValidationErrors = true
+    
+    console.log("FORM SUBMITTED")
+    console.log(form)
+    
+
+    this.dataService.addTodo(new Todo(form.value.text))
   }
 }
