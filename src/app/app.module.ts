fb1b315432b0f7aaf189438cae51591983641549
mdorizon <mdorizon17@gmail.com>
2024-01-10 18:08:59 +0100

    todolist removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 import { KanbanComponent } from './kanban/kanban.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,7 +13,6 @@ import { HomeComponent } from './pages/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent,
     KanbanComponent,
     KanbanPageComponent,
     HomeComponent
