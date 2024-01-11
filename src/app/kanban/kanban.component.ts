523aa73654c770e298fa656f603200ea3ebdb946
mdorizon <mdorizon17@gmail.com>
2024-01-11 11:11:04 +0100

    kanban tasks content WIP

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -7,50 +7,49 @@ import { Column } from '../models/column.model';
   selector: 'app-kanban',
   templateUrl: './kanban.component.html',
   styleUrl: './kanban.component.scss',
-
 })
 export class KanbanComponent {
 
   board: Board = new Board('Test board', [
     new Column('Ideas', [
-      "Some random idea",
-      "This is another random ideaThis is another random ideaThis is another random ideaThis is another random idea",
-      "build an awesome application"
+      {content: 'test', icon: 'test'},
+      // 'test',
+      // 'test'
     ],
     'gray'),
-    new Column('Todo', [
-      'Get to work',
-      'Pick up groceries',
-      'Go home',
-      'Fall asleep'
-    ],
-    'brown'),
-    new Column('Done', [
-      'Get up',
-      'Brush teeth',
-      'Take a shower',
-      'Check e-mail',
-      'Walk dog'
-    ],
-    'orange'),
-    new Column('Done', [
-    ],
-    'yellow'),
-    new Column('Done', [
-    ],
-    'green'),
-    new Column('Done', [
-    ],
-    'blue'),
-    new Column('Done', [
-    ],
-    'purple'),
-    new Column('Done', [
-    ],
-    'pink'),
-    new Column('Done', [
-    ],
-    'red')
+    // new Column('Todo', 
+    //   {content: 'test', icon: 'test'},
+    //   // new Task({ content: 'Get to work', icon: '🦖'}),
+    //   // new Task({ content: 'Pick up groceries', icon: '❤️'}),
+    //   // new Task({ content: 'Go home', icon: '🦖'}),
+    //   // new Task({ content: 'Fall asleep', icon: '❤️'})
+    // 'brown'),
+    // new Column('Done', 
+    //   {content: 'test', icon: 'test'},
+    //   // new Task({ content: 'Get up', icon: '🦖'}),
+    //   // new Task({ content: 'Brush teeth', icon: '❤️'}),
+    //   // new Task({ content: 'Take a shower', icon: '🦖'}),
+    //   // new Task({ content: 'Check e-mail', icon: '🦖'}),
+    //   // new Task({ content: 'Walk dog', icon: '❤️'})
+    // 'orange'),
+    // new Column('Done', 
+    //   {content: 'test', icon: 'test'},
+    // 'yellow'),
+    // new Column('Done', 
+    //   {content: 'test', icon: 'test'},
+    // 'green'),
+    // new Column('Done', 
+    //   {content: 'test', icon: 'test'},
+    // 'blue'),
+    // new Column('Done', 
+    //   {content: 'test', icon: 'test'},
+    // 'purple'),
+    // new Column('Done', 
+    //   {content: 'test', icon: 'test'},
+    // 'pink'),
+    // new Column('Done', 
+    //   {content: 'test', icon: 'test'},
+    // 'red')
   ]);
 
   drop(event: CdkDragDrop<string[]>) {
--- src/app/models/column.model.ts
+++ src/app/models/column.model.ts
@@ -1,3 +1,3 @@
 export class Column {
-  constructor(public name: string, public tasks: string[], public color: string) {}
+  constructor(public name: string, public tasks: any[], public color: string) {}
 }
